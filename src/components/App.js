00eb2d05355528a9fa299bb0ab00efff577a361e
Yash708262 <yash708262@gmail.com>
2025-10-01 09:36:25 +0530

    sf

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,53 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [fields, setFields] = useState([]);
+
+  // Add new field
+  const addField = () => {
+    const newField = { id: Date.now(), value: "" }; // unique id
+    setFields([...fields, newField]);
+  };
+
+  // Update field value
+  const updateField = (id, newValue) => {
+    setFields(fields.map(field => 
+      field.id === id ? { ...field, value: newValue } : field
+    ));
+  };
+
+  // Delete field
+  const deleteField = (id) => {
+    setFields(fields.filter(field => field.id !== id));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <button className="add" onClick={addField}>Add field</button>
 
-export default App
+      {fields.length === 0 ? (
+        <p>No fields in the form</p>
+      ) : (
+        fields.map(field => (
+          <div key={field.id}>
+            <input
+              type="text"
+              value={field.value}
+              onChange={(e) => updateField(field.id, e.target.value)}
+            />
+            <button
+              className="delete"
+              onClick={() => deleteField(field.id)}
+            >
+              Delete
+            </button>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default App;
